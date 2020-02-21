391c92b492a688abae52cb4feaa4d12a7b3ac4e2
Qafarli Tutu <qeferlitutu@gmail.com>
2020-02-21 10:07:15 -0800

    Homework8 was created

---
+++ src/main/java/Homework8/Human.java
@@ -0,0 +1,109 @@
+package Homework8;
+
+import java.util.*;
+
+public class Human {
+    private String name;
+    private String surname;
+    private int year;
+    private int iq;
+    private Pet pet;
+    Map schedule=new HashMap();
+
+    public Human() {
+    }
+
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+
+
+    public Human(String name, String surname, int year, int iq, Map schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.schedule = schedule;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public int getIq(int i) {
+        return iq;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public Map getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Map schedule) {
+        this.schedule = schedule;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", year=" + year +
+                ", iq=" + iq +
+                ", pet=" + pet +
+                ", schedule=" + schedule +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return year == human.year &&
+                iq == human.iq &&
+                Objects.equals(name, human.name) &&
+                Objects.equals(surname, human.surname) &&
+                Objects.equals(pet, human.pet) &&
+                Objects.equals(schedule, human.schedule);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, year, iq, pet, schedule);
+    }
+}
---
+++ src/main/java/Homework8/Main.java
@@ -0,0 +1,36 @@
+package Homework8;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Human child=new Human("Alex","Miller",2016);
+        Human child1=new Human("Martin","Miller",2019);
+        List<Human> children = new ArrayList<>();
+        children.add(child);
+        children.add(child1);
+        Set<String> habits1 = new HashSet<String>();
+        habits1.add("eating");
+        habits1.add("running");
+        Pet pet= new Pet("Dog", "Lucky", 2, 67, habits1 );
+        Set<String> habits2 = new HashSet<String>();
+        habits2.add("sleeking");
+        habits2.add("licking");
+        Pet pet1= new Pet("Cat","Nick",1,25,habits2);
+        Set<Pet> pets = new HashSet<>();
+        pets.add(pet);
+        pets.add(pet1);
+        Map schedule=new HashMap();
+        schedule.put("Monday","go work");
+        schedule.put("Sunday","go gym");
+        Human father= new Human("John", "Miller", 1973,90,schedule);
+        Human mother=new Human("Isabel", "Miller", 1971,85, schedule);
+        Family family=new Family(mother,father);
+        family.setChildren(children);
+        family.setPets(pets);
+        System.out.println(family);
+
+
+
+    }
+}
